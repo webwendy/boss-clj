12e9744d4ccdff6651b5e199a45fb78e5ecf1445
Dan Hammer <dan.s.hammer@gmail.com>
2013-01-11 19:45:55 -0800

    working loop example

--- src/clj/boss/query.clj
+++ src/clj/boss/query.clj
@@ -9,83 +9,49 @@
 (def sub-pool (range 5))
 (def pool     (range 100))
 
-(defn my-func [coll]
-  (prn coll)
-  [(reduce + coll)])
+(defn my-func
+  [coll]
+  (reduce + coll))
 
+(defn all-distinct?
+  [coll]
+  (= (count coll) (count (distinct coll))))
 
-(defn replace-chunk [[sub-vec]]
-  (vec (conj (take 4 (shuffle sub-vec))
-             (nth pool (rand-int 99)))))
+(defn replace-chunk
+  [coll new-chunk]
+  (let [n (- (count coll) (count new-chunk))
+        x (flatten (cons new-chunk
+                         (take n (shuffle coll))))]
+    (if (all-distinct? x) x nil)))
 
-(defmapcatop exploder [N sub-vec]
-  (let [f (fn [_]  (set (replace-chunk sub-vec)))]
-    (prn (vec
-          (map vec
-               (set (map f (range N))))))
-    [[(map vec
-           (set (map f (range N))))]]))
+(defn random-subgroup
+  [coll n]
+  (take n (shuffle coll)))
 
-(defn boss-query
-  [p-results]
-  (let [src [[[p-results]]]]
-    (??<- [?f-val]
-          (src ?a)
-          (exploder 10 ?a :> ?b)
-          (my-func ?b :> ?f-val))))
+(defn sub-cols [N n coll]
+  (let [f (fn [_] (set (random-subgroup coll n)))]
+    (map vec (distinct
+              (map f (range N))))))
 
-   ;; (??<- [?trial-vec]
-   ;;        (src ?orig-vec)
-   ;;        (explode-vec 10 ?orig-vec :> ?trial-vec)
-   ;;        (my-func ?trial-vec :> ?f-val)
-   ;;        (ops/max ?f-val))
+(defmapcatop explode-group [coll]
+  (let [chunks (sub-cols 100 2 pool)]
+    (vec
+     (for [c chunks]
+       [(vec (replace-chunk coll c))]))))
+ 
+(defn iter-step [prev-coll]
+  (??<- [?b ?c]
+        ([[prev-coll]] :> ?a)
+        (explode-group ?a :> ?b)
+        (my-func ?b :> ?c)))
 
+(defn process-res [prev-coll]
+  (ffirst (sort-by second > (iter-step prev-coll))))
 
-
-
-(def idx-group (set (range 20)))
-(def idx-pool  (set (range 100)))
-
-
-(defstruct obs :id :attr)
-
-
-
-(defn random-subgroup [idx-coll n]
-  (take n (shuffle idx-coll)))
-
-(defn grab-new [orig-coll new-coll n]
-  {:pre [(< n (count orig-coll))]}
-  (let [num-orig (- (count orig-coll) n)]
-    (concat (take num-orig orig-coll)
-            (take n ))))
-
-(defn mean-effect [src]
-  (<- [?ya]
-      (src ?x1 ?x2 ?x3)
-      (dgp ?x1 ?x2 ?x3 :> ?y)
-      (ops/avg ?y :> ?ya)))
-
-;; (defn distribute-proc []
-;;   (let [data (data-map 100000 500)
-;;         treatment-src (:treatment data)]
-;;     (reference-freq ?)))
-
-(defn impact-diff [src-c src-t]
-  (let [extract-mean (fn [src] (ffirst (??- (mean-effect src))))]
-    (i/abs (apply map - (map extract-mean [src-c src-t])))))
-
-(defn tester []
-  (let [data (data-map 100000 500)
-        treatment-src (:treatment data)
-        control-src (:control data)]
-    (impact-diff control-src treatment-src)))
-
-
-;; (defn boss-impact [B]
-;;   (let [data (data-map 100000 500)
-;;         treatment-src (:treatment data)
-;;         control-src (control-group (:control data) treatment-src B)]
-;;     (impact-diff control-src treatment-src)))
-
-
+(defn test-loop [orig-coll]
+  (loop [coll orig-coll
+         i 10]
+    (if (neg? i)
+      coll
+      (recur (process-res coll)
+             (dec i)))))
